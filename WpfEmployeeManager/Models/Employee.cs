74eb864821010548962f993206ef638378216c2b
Babiker <babikay.25@gmail.com>
2023-05-30 23:45:02 +0200

    Add project files.

---
+++ WpfEmployeeManager/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEmployeeManager.Models
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string status { get; set; }
+
+
+    }
+}
