6de3240953c980705bf0cc2640353b95f78c9fc9
Babiker <babikay.25@gmail.com>
2023-05-31 23:12:32 +0200

    added try/catch and responses

--- WpfEmployeeManager/MainWindow.xaml.cs
+++ WpfEmployeeManager/MainWindow.xaml.cs
@@ -45,47 +45,94 @@ namespace WpfEmployeeManager
 
         private async void GetEmployees()
         {
-            lblMessage.Content = "";
-            var response = await client.GetStringAsync("users");
-            var employees = JsonConvert.DeserializeObject<List<Employee>>(response);
-            dgEmployee.DataContext = employees;
+                lblMessage.Content = "";
+            try
+            {
+                var response = await client.GetStringAsync("users");
+                var employees = JsonConvert.DeserializeObject<List<Employee>>(response);
+                dgEmployee.DataContext = employees;
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Content = ex.Message;
+            }
         }
 
         private async void SaveEmployee(Employee employee)
         {
-            await client.PostAsJsonAsync("users", employee);
+            try
+            {
+                await client.PostAsJsonAsync("users", employee);
+
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Content = ex.Message;
+
+            }
         }
 
         private async void UpdateEmployee(Employee employee)
         {
-            await client.PutAsJsonAsync("users/"+ employee.id, employee);
+            try
+            {
+                await client.PutAsJsonAsync("users/" + employee.id, employee);
+
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Content = ex.Message;
+
+            }
         }
 
         private async void DeleteEmployee(int employeeId)
         {
-            await client.DeleteAsync("users/"+ employeeId);
+            try
+            {
+                await client.DeleteAsync("users/" + employeeId);
+                lblMessage.Content = "Employee Deleted";
+
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Content = "Error deleting employee";
+
+            }
         }
 
         private void BtnSaveEmployee_Click(object sender, RoutedEventArgs e)
         {
-            var employee = new Employee()
+            try
             {
-                id = Convert.ToInt32(txtEmployeeId.Text),
-                name = txtName.Text,
-                email = txtEmail.Text,
-                gender = txtGender.Text,
-                status = txtStatus.Text,
-            };
+                var employee = new Employee()
+                {
+                    id = Convert.ToInt32(txtEmployeeId.Text),
+                    name = txtName.Text,
+                    email = txtEmail.Text,
+                    gender = txtGender.Text,
+                    status = txtStatus.Text,
+                };
 
-            if (employee.id == 0)
-            {
-                this.SaveEmployee(employee);
-                lblMessage.Content = "Employee Saved";
+                if (employee.id == 0 && !string.IsNullOrEmpty(employee.name))
+                {
+                    this.SaveEmployee(employee);
+                    lblMessage.Content = "Employee Saved";
+
+                }
+                else if (employee.id != 0)
+                {
+                    this.UpdateEmployee(employee);
+                    lblMessage.Content = "Employee Updated";
+
+                }
+
+
             }
-            else
+            catch (Exception ex)
             {
-                this.UpdateEmployee(employee);
-                lblMessage.Content = "Employee Updated";
+                lblMessage.Content = "Error saving employee";
+
             }
 
             txtEmployeeId.Text = 0.ToString();
@@ -94,30 +141,42 @@ namespace WpfEmployeeManager
             txtGender.Text = "";
             txtStatus.Text = "";
 
-            GetEmployees();
 
         }
 
         void BtnEditEmployee(object sender, RoutedEventArgs e)
         {
-            Employee employee = ((FrameworkElement)sender).DataContext as Employee;
-       
-      
+            Employee? employee = ((FrameworkElement)sender).DataContext as Employee;
+            if (employee != null)
+            {
                 txtEmployeeId.Text = employee.id.ToString();
                 txtName.Text = employee.name;
                 txtEmail.Text = employee.email;
                 txtGender.Text = employee.gender;
                 txtStatus.Text = employee.status;
+               
+                SetEmployeeData(employee);
+            }
+              
+          
 
-            
-           
+        }
+
+        private void SetEmployeeData(Employee employee)
+        {
+            txtEmployeeId.Text = employee?.id.ToString();
+            txtName.Text = employee?.name;
+            txtEmail.Text = employee?.email;
+            txtGender.Text = employee?.gender;
+            txtStatus.Text = employee?.status;
         }
 
         void BtnDeleteEmployee(object sender, RoutedEventArgs e)
         {
-            Employee employee = ((FrameworkElement)sender).DataContext as Employee;
+            Employee? employee = ((FrameworkElement)sender).DataContext as Employee;
             this.DeleteEmployee(employee.id);
-            GetEmployees();
+            
+            this.GetEmployees();
         }
     }
 }
